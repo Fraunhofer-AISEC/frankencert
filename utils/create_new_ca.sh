61375a1caa1eacd8205fa5a41808d29e7ce754e9
Stefan Tatschner <stefan@rumpelsepp.org>
2020-02-05 11:18:57 +0100

    … and remove even more stuff

--- utils/create_new_ca.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-openssl genrsa -out tmpCA.key 1024
-openssl req -x509 -new -nodes -key tmpCA.key -days 1024 -out tmpCA.pem
-cat tmpCA.key > rootCA.pem
-cat tmpCA.pem >> rootCA.pem
-rm tmpCA.pem tmpCA.key
