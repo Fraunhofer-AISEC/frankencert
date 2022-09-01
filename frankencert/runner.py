2db514f432111135b3281099952379e86a5af1e7
Stefan Tatschner <stefan@rumpelsepp.org>
2022-09-01 09:16:35 +0200

    Add a runner to test certs based on gallia

--- frankencert/runner.py
+++ frankencert/runner.py
@@ -1,23 +1,26 @@
-#!/usr/bin/env python3
+from __future__ import annotations
 
-import asyncio
 import argparse
-import base64
-import gzip
 import json
 import multiprocessing
-import signal
+import sqlite3
+import subprocess
 import sys
-from asyncio import to_thread
+import warnings
+from abc import ABC, abstractmethod
+from argparse import ArgumentParser, Namespace
 from datetime import datetime
 from pathlib import Path
-from subprocess import PIPE
-from typing import Any, Optional, Iterable
+from subprocess import PIPE, Popen
+from typing import cast
 
-import aiosqlite
-from aiofiles.os import stat
-from aiofiles.ospath import exists, isdir
+from cryptography.utils import CryptographyDeprecationWarning
+from gallia.config import ConfigType, load_config_file
+from gallia.command import Script
 
+warnings.filterwarnings("ignore", category=CryptographyDeprecationWarning)
+
+from cryptography import x509
 
 SCHEMA_VERSION = 0
 SCHEMA = """
@@ -36,234 +39,281 @@ CREATE TABLE scan_run (
 CREATE TABLE scan_result (
     id integer primary key,
     run int not null references scan_run(id) on update cascade on delete cascade,
-    loader_path text not null,
+    loader text not null,
     start_time real not null,
     end_time real not null,
-    cert blob,
-    exit_code int not null,
-    stdout blob,
-    stderr blob
+    in_data text check(json_valid(in_data)),
+    out_data text check(json_valid(out_data))
 ) STRICT;
 """
 
 
-class Runner:
-    def __init__(self, db_path: Path, cert_file: Path, loaders: list[Path]) -> None:
-        self.db_path = db_path
-        self.cert_file = cert_file
-        self.loader_paths = loaders
-        self.loaders: dict[str, asyncio.subprocess.Process] = {}
-        self.run_id: Optional[int] = None
-        self.mutexes: dict[str, asyncio.Lock] = {}
+class BasePlugin(ABC):
+    @abstractmethod
+    def run(self, cert: bytes) -> None | str:
+        ...
 
-    async def commit(self) -> None:
-        await self.db.commit()
+    def __str__(self) -> str:
+        return self.__class__.__name__
 
-    async def close(self) -> None:
-        await self.db.commit()
-        await self.db.close()
+    def __repr__(self) -> str:
+        return str(self)
 
-    async def _do_sql_single(self, sql: str, args: Optional[Iterable] = None) -> None:
-        if args is not None:
-            await self._do_sql_multiple([sql], {0: args})
-        else:
-            await self._do_sql_multiple([sql])
 
-    async def _do_sql_multiple(
-        self, sql: list[str], args: Optional[dict[int, Iterable[Any]]] = None
-    ) -> None:
-        for i, line in enumerate(sql):
-            if args is not None and i in args:
-                await self.cur.execute(line, args[i])
-            else:
-                await self.cur.execute(line)
+class IPCPlugin(BasePlugin):
+    def __init__(self, path: Path):
+        self.path = path
+        self.loader = Popen([self.path], stdout=PIPE, stdin=PIPE, text=True, bufsize=1)
 
-    async def _db_connect(self) -> None:
-        self.db = await aiosqlite.connect(self.db_path)
-        self.cur = await self.db.cursor()
-        sql = [
-            "PRAGMA foreign_keys = ON;",
-            f"PRAGMA threads = {multiprocessing.cpu_count()};",
-        ]
-        await self._do_sql_multiple(sql)
+    def run(self, cert: bytes) -> None | str:
+        assert self.loader.stdout
+        assert self.loader.stdin
 
-    async def db_create(self) -> None:
-        await self._db_connect()
-        await self.cur.executescript(SCHEMA)
-        await self.cur.execute(
+        data = json.dumps({"in": cert.decode()})
+        self.loader.stdin.write(data + "\n")
+        res = json.loads(self.loader.stdout.readline())
+        if "out" in res and res["out"]:
+            return str(res["out"])
+        return None
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}({self.path})"
+
+
+class GNUTLS_Plugin(BasePlugin):
+    def run(self, cert: bytes) -> None | str:
+        p = subprocess.run(
+            [
+                "certtool",
+                "--certificate-info",
+                "--load-certificate",
+                "/dev/stdin",
+            ],
+            input=cert,
+            capture_output=True,
+        )
+        if p.returncode != 0:
+            return p.stderr.decode().strip()
+        return None
+
+
+class PythonPlugin(BasePlugin):
+    def run(self, cert: bytes) -> None | str:
+        try:
+            x509.load_pem_x509_certificate(cert)
+            return None
+        except Exception as e:
+            return str(e)
+
+
+class DBHandler:
+    def __init__(self, path: Path, db: sqlite3.Connection, cur: sqlite3.Cursor) -> None:
+        self.path = path
+        self.db = db
+        self.cur = cur
+
+    def commit(self) -> None:
+        self.db.commit()
+
+    def close(self) -> None:
+        self.db.commit()
+        self.db.close()
+
+    def create(self) -> None:
+        self.cur.executescript(SCHEMA)
+        self.cur.execute(
             "INSERT INTO version(version) VALUES(?)",
             (SCHEMA_VERSION,),
         )
 
-    async def db_connect(self) -> None:
-        await self._db_connect()
-        await self._do_sql_single("SELECT version from version;")
-        res = await self.cur.fetchone()
-        if res is None:
-            raise RuntimeError("no schema version")
-        if (schema_version := res[0]) != SCHEMA_VERSION:
-            raise RuntimeError(f"unsupported schema version: {schema_version}")
+    @classmethod
+    def connect(cls, path: Path) -> DBHandler:
+        create = False if path.exists() else True
 
-    async def run_add(self, command: list[str], start_time: datetime) -> None:
-        await self._do_sql_single(
+        db = sqlite3.connect(path)
+        cur = db.cursor()
+        # https://phiresky.github.io/blog/2020/sqlite-performance-tuning/
+        sql = [
+            "PRAGMA foreign_keys = ON;",
+            f"PRAGMA threads = {multiprocessing.cpu_count()};",
+            "PRAGMA journal_mode = WAL;",
+            "PRAGMA synchronous = normal;",
+            "PRAGMA temp_store = memory;",
+        ]
+        for line in sql:
+            cur.execute(line)
+
+        if create:
+            cur.executescript(SCHEMA)
+            cur.execute(
+                "INSERT INTO version(version) VALUES(?)",
+                (SCHEMA_VERSION,),
+            )
+            db.commit()
+        else:
+            cur.execute("SELECT version from version;")
+            res = cur.fetchone()
+            if res is None:
+                raise RuntimeError("no schema version")
+            if (schema_version := res[0]) != SCHEMA_VERSION:
+                raise RuntimeError(f"unsupported schema version: {schema_version}")
+
+        return cls(path, db, cur)
+
+    def run_add(self, command: list[str], start_time: datetime) -> None:
+        self.cur.execute(
             "INSERT INTO scan_run(command, start_time) VALUES(?, ?)",
             (json.dumps(command), start_time.timestamp()),
         )
         self.run_id = self.cur.lastrowid
 
-    async def run_finish(self, end_time: datetime, exit_code: int) -> None:
+    def run_finish(self, end_time: datetime, exit_code: int) -> None:
         assert self.run_id, "run_id is not set"
-        await self._do_sql_single(
+        self.cur.execute(
             "UPDATE scan_run SET end_time=?, exit_code=? WHERE id==?",
             (end_time.timestamp(), exit_code, self.run_id),
         )
 
-    async def result_add(
+    def result_add(
         self,
-        loader_path: Path,
+        loader: str,
         start_time: datetime,
         end_time: datetime,
-        cert: bytes,
-        exit_code: int,
-        stdout: bytes,
-        stderr: bytes,
+        in_data: dict,
+        out_data: dict,
     ) -> int:
         assert self.run_id, "run_id is not set"
-        await self._do_sql_single(
+        self.cur.execute(
             """INSERT INTO scan_result(
                     run,
-                    loader_path,
+                    loader,
                     start_time,
                     end_time,
-                    exit_code,
-                    cert,
-                    stdout,
-                    stderr
-                ) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+                    in_data,
+                    out_data
+                ) VALUES(?, ?, ?, ?, ?, ?)""",
             (
                 self.run_id,
-                str(loader_path),
+                loader,
                 start_time.timestamp(),
                 end_time.timestamp(),
-                exit_code,
-                cert,
-                stdout,
-                stderr,
+                json.dumps(in_data),
+                json.dumps(out_data),
             ),
         )
+        assert self.cur.lastrowid
         return self.cur.lastrowid
-
-    async def _testrun_call(self, loader: str, cert: bytes) -> None:
-        assert self.run_id, "run_id is not set"
-        start_time = datetime.now()
-        p = self.loaders[loader]
-        assert p.stdin, "stdin of loader is not connected"
-        assert p.stdout, "stdout of loader is not connected"
-        assert p.stderr, "stderr of loader is not connected"
-
-        p.stdin.write(cert)
-        await p.stdin.drain()
-        end_time = datetime.now()
-
-        print("boo")
-        stderr = await p.stderr.feed_data read(64 * 1024)
-        print("boo")
-        stdout = await p.stdout.readline()
-        ipc_json = json.loads(stdout)
-        returncode = ipc_json["return_code"]
-        print(returncode)
-
-        await self.result_add(
-            Path(loader),
-            start_time,
-            end_time,
-            cert,
-            returncode,
-            stdout,
-            stderr,
-        )
-
-    async def testrun_call(self, loader: str, cert: bytes) -> None:
-        async with self.mutexes[loader]:
-            await self._testrun_call(loader, cert)
-
-    async def start_loaders(self) -> None:
-        for path in self.loader_paths:
-            p = await asyncio.create_subprocess_exec(
-                path, stdout=PIPE, stderr=PIPE, stdin=PIPE
-            )
-            self.loaders[str(path)] = p
-            self.mutexes[str(path)] = asyncio.Lock()
-
-    async def stop_loaders(self) -> None:
-        for k, v in self.loaders.items():
-            print(f"killing: {k}")
-            v.terminate()
-            await v.wait()
-
-    async def process_tests(self) -> None:
-        await self.run_add(sys.argv, datetime.now())
-        await self.start_loaders()
-
-        with await to_thread(gzip.open, self.cert_file, "r") as f:
-            while True:
-                line = await to_thread(f.readline)
-                if line == b"":
-                    break
-                cert = parse_line(line.decode())
-
-                for loader in self.loaders:
-                    await self.testrun_call(loader, cert)
 
 
 def parse_line(line: str) -> bytes:
     _, cert = line.split(",", maxsplit=1)
-    b = base64.standard_b64decode(cert)
-    out = base64.standard_b64encode(b)
-    return b"-----BEGIN CERTIFICATE-----\n" + out + b"\n-----END CERTIFICATE-----\n"
+    return (
+        b"-----BEGIN CERTIFICATE-----\n"
+        + cert.encode()
+        + b"\n-----END CERTIFICATE-----\n"
+    )
 
 
-async def load_loaders(path: Path) -> list[Path]:
-    res = []
-    for loader in path.iterdir():
-        if await isdir(loader) or (await stat(loader)).st_mode & 0o100 != 0o100:
-            continue
-        res.append(loader)
-    return res
+class Runner(Script):
+    COMMAND = "frankencert"
+    LOGGER_NAME = "frankenrunner"
+
+    def __init__(self, parser: ArgumentParser, config: ConfigType) -> None:
+        super().__init__(parser, config)
+        self.db: DBHandler
+
+    def add_parser(self) -> None:
+        self.parser.add_argument(
+            "--plugin-path",
+            type=Path,
+            metavar="PATH",
+            help="path to executables used as loaders",
+            default=self.get_config_value("frankencert.plugin_path", None),
+        )
+        self.parser.add_argument(
+            "--db",
+            type=Path,
+            help="path to sqlite database",
+            default=self.get_config_value("frankencert.db_path", None),
+        )
+
+    def load_ipc_plugins(self, path: Path) -> list[BasePlugin]:
+        out: list[BasePlugin] = []
+        for loader in path.iterdir():
+            if loader.is_dir():
+                continue
+            s = loader.stat()
+            if s.st_mode & 0o100 or s.st_mode & 0o010 or s.st_mode & 0o001:
+                out.append(IPCPlugin(loader))
+        return out
+
+    def process_tests(self, args: Namespace) -> None:
+        self.db.run_add(sys.argv, datetime.now())
+
+        plugins: list[BasePlugin] = [
+            GNUTLS_Plugin(),
+            PythonPlugin(),
+        ]
+
+        if (p := args.plugin_path) is not None:
+            plugins += self.load_ipc_plugins(p)
+
+        self.logger.info(f"loaded plugins: {plugins}")
+
+        for i, line in enumerate(sys.stdin):
+            if i % 1000 == 0:
+                self.logger.info(f"parsing cert #{i}")
+            cert = parse_line(line)
+
+            for plugin in plugins:
+                self.logger.trace(f"running plugin {plugin}")
+                start_time = datetime.now()
+                res = plugin.run(cert)
+                end_time = datetime.now()
+
+                # Do not log successfully parsed runs.
+                if res is None:
+                    continue
+
+                self.logger.info(f"{plugin}: error: {res}")
+                in_data = {"in": cert.decode()}
+                out_data = {"out": res}
+
+                self.db.result_add(
+                    loader=str(plugin),
+                    in_data=in_data,
+                    out_data=out_data,
+                    start_time=start_time,
+                    end_time=end_time,
+                )
+
+    def main(self, args: Namespace) -> None:
+        self.db = DBHandler.connect(args.db)
+        self.process_tests(args)
+
+    def entry_point(self, args: Namespace) -> int:
+        exit_code = 0
+
+        try:
+            exit_code = super().entry_point(args)
+        except Exception as e:
+            self.logger.exception(f"exception occured: {e}")
+            exit_code = 1
+        finally:
+            self.db.run_finish(datetime.now(), exit_code)
+            self.db.commit()
+            self.db.close()
+
+        return exit_code
 
 
-async def main() -> None:
+def main() -> None:
+    config, _ = load_config_file()
+
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--loader-dir", type=Path, help="Path to executables used as loaders"
-    )
-    parser.add_argument(
-        "--cert-file", type=Path, help="Path to certificat file to be loaded"
-    )
-    parser.add_argument("--db", type=Path, help="Path to sqlite database")
-    args = parser.parse_args()
-
-    loaders = await load_loaders(args.loader_dir)
-    runner = Runner(args.db, args.cert_file, loaders)
-    if await exists(args.db):
-        await runner.db_connect()
-    else:
-        await runner.db_create()
-
-    exit_code = 0
-
-    try:
-        await runner.process_tests()
-    except Exception:
-        raise
-    except KeyboardInterrupt:
-        exit_code = 128 + signal.SIGINT
-    finally:
-        await runner.run_finish(datetime.now(), exit_code)
-        await runner.close()
-
-    sys.exit(exit_code)
+    runner = Runner(parser, config)
+    sys.exit(runner.entry_point(parser.parse_args()))
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
